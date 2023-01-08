f229886a0b27c4f45f7b0df983f6c5850b076aae
Raghav <raghavtechlover@gmail.com>
2023-01-08 15:17:51 +0530

    Initial commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+//Homescreen is under development and is yet to be added. The rest odt the app works fine.
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.png'),
+          ),
+        ),
+        child: Column(
+          children: [
+            Text('Destini: Choose Your Own Odyssey'),
+            Container(
+              child: Image.asset('assets/images/logo.png'),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              onPressed: () {
+                setState(() {});
+              },
+              child: Text(
+                'Start the journey',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
