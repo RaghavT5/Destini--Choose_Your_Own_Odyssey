cb9c6747fa29e7c07b9b6142668e6935023a4657
Raghav <raghavtechlover@gmail.com>
2023-01-17 12:52:45 +0530

    Fixed homescreen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,5 @@
+import 'package:destini/story_screen.dart';
 import 'package:flutter/material.dart';
-
-//This screen is under development and is yet to be added. The rest odt the app works fine.
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -16,31 +15,61 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/images/background.png'),
-          ),
+              image: AssetImage('assets/images/background.png'),
+              fit: BoxFit.cover),
         ),
-        child: Column(
-          children: [
-            Text('Destini: Choose Your Own Odyssey'),
-            Container(
-              child: Image.asset('assets/images/logo.png'),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.blue,
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              SizedBox(
+                height: 60,
               ),
-              onPressed: () {
-                setState(() {});
-              },
-              child: Text(
-                'Start the journey',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
+              Expanded(
+                flex: 25,
+                child: Center(
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    'Destini: Choose Your Own Odyssey',
+                    style: TextStyle(
+                      fontFamily: 'Cinzel Decorative',
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ),
-            )
-          ],
+              Expanded(
+                flex: 150,
+                child: Container(
+                  child: Image.asset('assets/icons/icon.png'),
+                ),
+              ),
+              Expanded(
+                flex: 23,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                  child: Text(
+                    'Start the journey',
+                    style: TextStyle(
+                      fontFamily: 'Cinzel',
+                      color: Colors.white,
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => StoryScreen()));
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
